898d488ac6f1a7993979d373d55d358b9ed24f3e
HwangSukHyeon <payback9488@gmail.com>
2024-06-13 22:30:58 +0900

    [File]: Add TaskListView
    - 홈_할일 추가 완료 화면

---
+++ Boong-To-Do/TaskListView.swift
@@ -0,0 +1,18 @@
+//
+//  TaskListView.swift
+//  Boong-To-Do
+//
+//  Created by 황석현 on 6/13/24.
+//
+
+import SwiftUI
+
+struct TaskListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TaskListView()
+}
