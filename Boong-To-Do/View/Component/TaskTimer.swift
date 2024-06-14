3dd2fbc65b48c80337992d186ffc13699f29d6c3
HwangSukHyeon <payback9488@gmail.com>
2024-06-14 18:21:03 +0900

    [File&Anno]: 할일 상세보기(타이머)화면 파일 추가 & 주석 수정

---
+++ Boong-To-Do/View/Component/TaskTimer.swift
@@ -0,0 +1,18 @@
+//
+//  TaskTimer.swift
+//  Boong-To-Do
+//
+//  Created by 황석현 on 6/14/24.
+//
+
+import SwiftUI
+
+struct TaskTimer: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TaskTimer()
+}
