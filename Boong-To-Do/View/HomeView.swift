1ba3c3b4a236738a4660eb80de8cf71f956061e8
HwangSukHyeon <payback9488@gmail.com>
2024-06-21 21:03:40 +0900

    [Code]: 조건문 수정

--- Boong-To-Do/View/HomeView.swift
+++ Boong-To-Do/View/HomeView.swift
@@ -39,7 +39,7 @@ struct HomeView: View {
                 .onAppear {
                     viewModel.getTaskStates()
                 }
-            if !viewModel.notCompleteTasks.isEmpty || !viewModel.completeTasks.isEmpty {
+            if !viewModel.notCompleteTasks.isEmpty && !viewModel.completeTasks.isEmpty {
                 TaskListView(viewModel: viewModel)
             } else {
                 EmptyListView()
