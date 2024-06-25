1aca4cc3a5d6a706a63fcab9a9c5a48acb4ab623
HwangSukHyeon <payback9488@gmail.com>
2024-06-25 19:49:49 +0900

    [File]: 할일 완료 시 메모를 작성하는 화면 추가
    - CompletionMemo.swift

---
+++ Boong-To-Do/View/Component/CompletionMemo.swift
@@ -0,0 +1,19 @@
+//
+//  CompletionMemo.swift
+//  Boong-To-Do
+//
+//  Created by 황석현 on 6/25/24.
+//
+
+import SwiftUI
+
+/**할일 완료시 메모를 작성하는 화면*/
+struct CompletionMemo: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CompletionMemo()
+}
